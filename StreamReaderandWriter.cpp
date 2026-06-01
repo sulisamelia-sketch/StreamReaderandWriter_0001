e92776360eb7f7baf03aa8ee4cc1cfee9173ee63
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-01 10:15:31 +0700

    membuka file dalam mode menulis dan menunjuk ke sebuah nama file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -9,4 +9,11 @@ int main()
   //deklarasi variabel baris untuk menyimpan data string
     string baris;
 
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
