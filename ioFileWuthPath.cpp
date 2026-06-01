269e2e3f21b8e56a3573076b694e3d19887c4c39
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-01 10:23:39 +0700

    membuat file dalam mode menulis dan menunjuk ke sebuah nama file dengan ekstensi txt

--- ioFileWuthPath.cpp
+++ ioFileWuthPath.cpp
@@ -13,4 +13,11 @@ int main()
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
 
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstensi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     
