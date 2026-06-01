39cf00104153767710a1ebdec7abb92ace0f5606
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-01 10:18:27 +0700

    membuka file dalam mode membaca dan menunjuk ke sebuah file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -29,4 +29,10 @@ int main()
     //menutup file setelah penulisan selesai
     outfile.close();
 
-    
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file" << endl;
+   
