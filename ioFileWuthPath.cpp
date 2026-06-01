284dee1ee5c341db4994ce8fd0ff3f66dd139ec0
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-01 10:26:22 +0700

    menutup file apabila telah selesai di tulis dan membuka file dalam mode membaca

--- ioFileWuthPath.cpp
+++ ioFileWuthPath.cpp
@@ -37,4 +37,8 @@ int main()
     //membuka file dalam mode membaca
     ifstream infile;
 
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
     
