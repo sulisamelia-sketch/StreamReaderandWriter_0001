c5845ef8b3f980dd61043926af7349b9aa29b5d8
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-06-01 10:19:06 +0700

    kondisi jika file ada

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -35,4 +35,14 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">=Membuka dan membaca file" << endl;
-   
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
+    }
+ 
